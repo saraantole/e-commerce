8db4b1a3b90694ad6227ab99cdf7dadf9956fe59
saraa34 <sara.antole@gmail.com>
2020-11-16 14:39:56 +0100

    shop component and routing

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import { withRouter } from 'react-router-dom';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+    return (
+        <div className={`${size} menu-item`} onClick={() => { history.push(`${match.url}${linkUrl}`) }}>
+            <div className='bg-img' style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className='content'>
+                <h1 className='title'>{title}</h1>
+                <span className='subtitle'>Shop now</span>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(MenuItem);
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import MenuContainer from '../../components/menu-container/menu-container.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <MenuContainer />
+        </div>
+    )
+}
+
+export default HomePage;
