0dfb1ebd899cdee1a881755b9b9c5e3ddbca266d
saraa34 <sara.antole@gmail.com>
2020-12-03 15:34:16 +0100

    created backend for stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,26 @@
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
+import { json } from 'body-parser';
 
 const StripeCheckoutButton = ({ price }) => {
+
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51Hr5S5BjcqCIrEULrP533aR8EmB2amx2exA4XgaV3MHBHR4ngbTKREKYX3bAN0ox6y9Hqdo0o5BJHi08Z3vCgEmf00QZHV8YNz';
+
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res => {
+            alert('Payment Successful')
+        }).catch(err => {
+            console.log('Payment Error: ', JSON.parse(err));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.');
+        })
     };
 
     return (
